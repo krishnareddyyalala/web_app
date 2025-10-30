5a233343dd92491b189bc830d1d9fe85703bcf94
krishnareddyyalala <krishnareddyyalala2002@gmail.com>
2025-10-30 16:39:11 +0530

    Fix Render deployment - bind to 0.0.0.0

--- app.py
+++ app.py
@@ -234,6 +234,9 @@ def signup():
 
   return render_template('signup.html')
 
-if __name__== '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
